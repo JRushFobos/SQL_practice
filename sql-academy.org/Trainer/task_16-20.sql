200db860e078cd351aaacafbef4f37cb92caa211
JRushFobos <fobos_media@mail.ru>
2024-02-05 02:03:49 +0500

    add 16 - 20 tasks

--- sql-academy.org/Trainer/task_16-20.sql
+++ sql-academy.org/Trainer/task_16-20.sql
@@ -7,11 +7,20 @@ GROUP BY Passenger.name
 ORDER BY
     count DESC, Passenger.name ASC
 ------------------------------------------------------
---Task №17*
-
+--Task №17**
+SELECT  FamilyMembers.member_name as member_name,
+        FamilyMembers.status as status,
+        SUM(Payments.unit_price * Payments.amount) as costs
+FROM FamilyMembers
+JOIN Payments on FamilyMembers.member_id = Payments.family_member
+WHERE YEAR(Payments.date) = 2005
+GROUP BY FamilyMembers.member_name, FamilyMembers.status
 ------------------------------------------------------
---Task №18*
-
+--Task №18**
+SELECT member_name
+FROM FamilyMembers
+ORDER BY birthday
+LIMIT 1
 ------------------------------------------------------
 --Task №19*
 SELECT DISTINCT status
@@ -19,6 +28,13 @@ FROM FamilyMembers
 JOIN Payments on FamilyMembers.member_id = Payments.family_member
 where Payments.good = 9
 ------------------------------------------------------
---Task №20*
-
+--Task №20**
+SELECT FamilyMembers.status as status,
+       FamilyMembers.member_name as member_name,
+       SUM(Payments.amount*Payments.unit_price) as costs
+FROM FamilyMembers
+JOIN Payments on FamilyMembers.member_id = Payments.family_member
+JOIN Goods on Payments.good = Goods.good_id
+WHERE Goods.type = 4
+GROUP by FamilyMembers.status, FamilyMembers.member_name
 ------------------------------------------------------
